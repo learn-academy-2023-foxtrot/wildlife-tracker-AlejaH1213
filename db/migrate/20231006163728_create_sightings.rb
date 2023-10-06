05b208c878568339bdfceff260e07a2bcd689094
AlejaH1213 <alejajara2@gmail.com>
2023-10-06 10:10:06 -0700

    second story done

---
+++ db/migrate/20231006163728_create_sightings.rb
@@ -0,0 +1,12 @@
+class CreateSightings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :sightings do |t|
+      t.integer :animal_id
+      t.decimal :latitude
+      t.decimal :longitude
+      t.datetime :date
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_05_230521) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_06_163728) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "animals", force: :cascade do |t|
     t.string "common_name"
     t.string "scientific_binomial"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sightings", force: :cascade do |t|
+    t.integer "animal_id"
+    t.decimal "latitude"
+    t.decimal "longitude"
+    t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
